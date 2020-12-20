3fe65645df3e0830c407cb5a5fab852ee2bdfca2
JabriHassane <mrhassan.jabri@gmail.com>
2020-12-20 22:35:46 +0100

    modification

--- index.js
+++ index.js
@@ -22,3 +22,12 @@ $(document).ready(function() {
         }
     });
 });
+
+
+$(window).scroll(function() {
+    if ($(window).scrollTop()) {
+        $("nav").addClass("black");
+    } else {
+        $("nav").removeClass("black");
+    }
+});
